853c3f570d567b39daff3ea78dcea7ad324451ca
Andrii Pyroh <andrewpyrih@gmail.com>
2024-03-19 09:52:59 +0200

    Coppied favourite_places project from local machine

---
+++ favourite_places/lib/providers/user_places.dart
@@ -0,0 +1,16 @@
+import 'package:favourite_places/models/place.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+  UserPlacesNotifier() : super(const []);
+
+  void addPlace(Place place) {
+    final newPlace = Place(title: place.title, image: place.image);
+    state = [newPlace, ...state];
+  }
+}
+
+final userPlacesProvider =
+    StateNotifierProvider<UserPlacesNotifier, List<Place>>(
+  (ref) => UserPlacesNotifier(),
+);
---
+++ favourite_places/lib/screens/places.dart
@@ -0,0 +1,38 @@
+import 'package:favourite_places/providers/user_places.dart';
+import 'package:favourite_places/screens/add_place.dart';
+import 'package:favourite_places/widgets/places_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class PlacesScreen extends ConsumerWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  Widget build(context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlacesProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const AddPlaceScreen(),
+                ),
+              );
+            },
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12),
+        child: PlacesList(
+          places: userPlaces,
+        ),
+      ),
+    );
+  }
+}
